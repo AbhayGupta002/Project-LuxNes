3811f773e8c2510fba897e7baa69bc1b0864ad8e
Abhay Gupta <ag4035737@gmail.com>
2025-06-16 21:06:41 +0530

    Create BookingDetailsRepository.java

---
+++ backend/src/main/java/com/example/demo/repository/BookingDetailsRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface BookingDetailsRepository extends JpaRepository {
+
+}

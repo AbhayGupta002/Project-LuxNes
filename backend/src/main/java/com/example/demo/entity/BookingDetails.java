66d96842fb2a5036ef00ceb44e66f0daf190a9b3
Abhay Gupta <ag4035737@gmail.com>
2025-06-17 14:04:30 +0530

    Update BookingDetails.java

--- backend/src/main/java/com/example/demo/entity/BookingDetails.java
+++ backend/src/main/java/com/example/demo/entity/BookingDetails.java
@@ -1,8 +1,6 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +13,70 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "booking_details")
 public class BookingDetails {
+
+    @OneToOne
+    @JoinColumn(name = "registrationid")
+    private HotelDetails hotelDetails;
+
+    @Column(name = "registration_number")
+    private Long registrationNo;
+
+    @Column(name = "booking_id")
+    private Long bookingId;
+
+    @Column(name = "room_no")
+    private int roomNo;
+
+    @Column(name = "registered_location")
+    private Double registeredLocation;
+
+    @Column(name = "room_rent")
+    private int roomRent;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "hotelname")
+    private String hotelName;
+
+    @Column(name = "city")
+    private String city;
+
+
+
+
+    @OneToOne
+    @JoinColumn(name = "userId")
+    private  UserDetails userDetails;
+
     @Column(name = "userid", unique = true,nullable = false)
     private Long userid;
 
-    @Column(name = "gmail")
-    private  String gmail
+    @Column(name = "gmail", nullable = false)
+    private String gmail;
+
+    @Column(name = "contact_number", nullable = false)
+    private Long contactNumber;
+
+    @Column(name = "username", nullable = false)
+    private String name;
+
+    @Column(name = "payment", nullable = false)
+    private String paymentMode;
+
+    @Column(name = "visitors", nullable = false)
+    private int totalVisitors;
+
+    @Column(name = "time_duration", nullable = false)
+    private String time;
+
+    @Column(name = "boarding_time", nullable = false,updatable = false)
+    private String dateTime;
+
+    @Column(name = "leave_time", nullable = false,updatable = false)
+    private String leaveTime;
+
+    @Column(name = "status", nullable = false, updatable = false)
+    private String status;
+
 }

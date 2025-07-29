a42f49c665bf253ebc4810cf5592552568c01d64
Abhay Gupta <ag4035737@gmail.com>
2025-07-29 18:59:46 +0530

    Update UserDto.java

--- backend/src/main/java/com/example/demo/dto/UserDto.java
+++ backend/src/main/java/com/example/demo/dto/UserDto.java
@@ -1,4 +1,23 @@
 package com.example.demo.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
 public class UserDto {
+    public String getHotelId;
+    private String bookingId;
+    private String userId;
+    private String name;
+    private String contactNumber;
+    private String gmail;
+    private String password;
+    private String Location;
+
 }

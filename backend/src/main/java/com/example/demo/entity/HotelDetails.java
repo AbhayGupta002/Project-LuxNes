dcb8f274f70a8a45f3b411ade22b25a0dd473ddb
Abhay Gupta <ag4035737@gmail.com>
2025-07-29 19:01:58 +0530

    Update HotelDetails.java

--- backend/src/main/java/com/example/demo/entity/HotelDetails.java
+++ backend/src/main/java/com/example/demo/entity/HotelDetails.java
@@ -1,43 +1,48 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
 @Entity
-@Table(name = "hotelregistered")
-public class HotelRegistered {
+@Table(name = "hotel_details")
+public class HotelDetails {
 
     @Id
-    @Column(name = "Hregistrationid")
-    private Long registrationId;
+    @Column(name = "hotel_id")
+    private String hotelId;
 
-    @Column(name = "Hhotelname", nullable = false)
+    @Column(name = "hotel_name", nullable = false)
     private String hotelName;
 
-    @Column(name = "Hphonenumber", nullable = false, unique = true)
-    private Long phoneNumber;
+    @Column(name = "phone_number", nullable = false, unique = true)
+    private String phoneNumber;
 
-    @Column(name = "registeredlocation", nullable = false)
-    private Double location;
+    @Column(name = "registered_location")
+    private String location;
 
-    @Column(name = "Hgmail", nullable = false)
+    @Column(name = "gmail", nullable = false)
     private String gmail;
 
-    @Column(name = "Hrooms", nullable = false)
-    private String rooms;
+    @OneToOne(mappedBy = "hotelDetails", cascade = CascadeType.ALL)
+    private UserLogin userLogin;
 
-    @Column(name = "Htype", nullable = false)
-    private String type;
+    @Column(name = "available_rooms")
+    private String availableRooms;
 
-    @Column(name = "Hcity", nullable = false)
+    @Column(name = "type", nullable = false)
+    private String hotelType;
+
+    @Column(name = "city")
     private String city;
 
+    @Column(name = "password")
+    private String password;
 }
